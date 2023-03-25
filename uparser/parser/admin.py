7ab49e5b09009ce9e1563b854234e7a2bc6281ad
wrawka <wrawka@gmail.com>
2023-03-25 12:55:16 +0700

    django model finalized

--- uparser/parser/admin.py
+++ uparser/parser/admin.py
@@ -5,4 +5,16 @@ from parser.models import OrderLine
 
 @admin.register(OrderLine)
 class OrderLineAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'line_no',
+        'order_no',
+        'due_date',
+        'value_usd',
+        'value_rub'
+    )
+    search_fields = ('order_no', 'due_date')
+    fields = (
+        ('line_no', 'order_no'),
+        'due_date',
+        ('value_usd', 'value_rub')
+    )
